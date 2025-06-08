1789d32fdddd8cb48c2d55e2fc237409fceb67d0
Hatori <ahgangadm@gmail.com>
2025-06-08 22:15:32 +0300

    Create full logic for App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,44 @@
 import {Toaster} from "react-hot-toast";
 import {lazy, Suspense, useEffect} from "react";
 import {Routes, Route, useLocation} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import {selectIsRefreshing} from "../../redux/auth/selectors.js";
+import {refresh} from "../../redux/auth/operations.js";
 
+import Loader from "../Loader/Loader.jsx";
+import LayoutApp from "../LayoutApp/LayoutApp.jsx";
+
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
+const SignInPage = lazy(() => import("../../pages/SignInPage/SignInPage.jsx"));
+const SignUpPage = lazy(() => import("../../pages/SignUpPage/SignUpPage.jsx"));
+const SearchPage = lazy(() => import("../../pages/SearchPage/SearchPage.jsx"));
+const NotFoundPage = lazy(() => import("../../pages/NotFoundPage/NotFoundPage.jsx"));
+
+const App = () => {
+    const isRefreshing= useSelector(selectIsRefreshing);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(refresh());
+    }, [dispatch]);
+    return (
+        <>
+            {isRefreshing ? (<Loader />) : (
+        <LayoutApp>
+            <Suspense fallback={<Loader />}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/signin" element={<SignInPage />} />
+                    <Route path='/search' element={<SearchPage />} />
+                    <Route path='*' element={<NotFoundPage />} />
+                </Routes>
+            </Suspense>
+        </LayoutApp>
+
+            )}
+        </>
+    );
+}
+
+export default App;
