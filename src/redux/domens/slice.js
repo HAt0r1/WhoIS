b06621228f28554f703d802a3354b9bd77bae093
Hatori <ahgangadm@gmail.com>
2025-06-11 21:28:37 +0300

    Create slice for domens

---
+++ src/redux/domens/slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {searchDomain} from "./operations.js";
+
+const initialState = {
+    result: null,
+    isLoading: false,
+    error: null,
+};
+
+const domainSlice = createSlice({
+    name: "domain",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(searchDomain.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(searchDomain.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.result = action.payload;
+            })
+            .addCase(searchDomain.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export default domainSlice.reducer;
