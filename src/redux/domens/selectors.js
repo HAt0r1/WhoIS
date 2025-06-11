a455ccc31b05ee4bd728764b571362dec2de4a27
Hatori <ahgangadm@gmail.com>
2025-06-11 21:28:52 +0300

    Create selectors for domens

---
+++ src/redux/domens/selectors.js
@@ -0,0 +1,3 @@
+export const selectResult = (state) => state.domain.result;
+export const selectIsLoading = (state) => state.domain.isLoading;
+export const selectError = (state) => state.domain.error;
