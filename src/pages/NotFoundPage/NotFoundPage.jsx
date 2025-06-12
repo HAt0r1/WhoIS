bbad4da1826230022a153732f44421621c025cd4
Hatori <ahgangadm@gmail.com>
2025-06-12 22:22:41 +0300

    Update markup for NotFoundPage component

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,15 @@
 import {NavLink} from 'react-router-dom'
 
+import css from "./NotFoundPage.module.css";
 
 
 const NotFoundPage = () => {
     return (
-        <div>
+        <main className={css.main}>
             <h2>Sorry, something went wrong &#128532;</h2>
             <p>You can reload the page and click the button below to return to the home page.</p>
-            <NavLink to="/">HomePage</NavLink>
-        </div>
+            <NavLink className={css.button} to="/">Home Page</NavLink>
+        </main>
     )
 }
 
